1305b4df5af4bbca3b1944654e46e2e96b6c4144
alspok <alspok@gmail.com>
2019-03-14 13:22:33 +0200

    some changes

--- resources/views/admin/products/temp.blade.php
+++ resources/views/admin/products/temp.blade.php
@@ -54,6 +54,9 @@
 							<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
 						</tr> -->
 					</tbody>
+					<tfoot>
+					
+					</tfoot>
 				</table>
 			</div>
 		</div>
