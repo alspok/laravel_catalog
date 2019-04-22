1e7c4f8e9059acca7638ccc1c6f96e515ad3a230
alspok <alspok@gmail.com>
2019-04-22 22:39:01 +0300

    .

--- resources/views/mail/food_mail_html.blade.php
+++ resources/views/mail/food_mail_html.blade.php
@@ -1,17 +1,17 @@
 Labas, <i>{{ $foodMail->receiver }}</i>,
-<p>Užsakymas</p>
+<p>Užsakymas {{$foodMail->currentTime}}</p>
 <p>Maistas</p>
     @foreach($foodMail->tableView as $item)
-        @if($item->shop_id == 2 && $item->category == 1)
-            {{$item->name}}<br>
+        @if($item->shop_id == 1 && $item->category == 1)
+            {{$item->name}}    {{$item->weight}}    {{$item->unit}}<br>
         @endif
     @endforeach
 <p>Kita</p>
     @foreach($foodMail->tableView as $item)
-        @if($item->shop_id == 1 && $item->category == 2)
-            {{$item->name}}<br>
+        @if($item->shop_id == 2 && $item->category == 2)
+        {{$item->name}}    {{$item->weight}}    {{$item->unit}}<br>
         @endif
     @endforeach
-<br><br>
+<br>
 Dėkui.<br>
 <i>{{ $foodMail->sender }}</i>
