365556a7d5a0159bcbed49d924dc9f4fef1a0ff3
alspok <alspok@gmail.com>
2019-04-22 19:42:45 +0300

    .

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,16 +6,20 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Mail\FoodEmail;
 use Illuminate\Support\Facades\Mail;
+use DB;
 use Auth;
 
 class MailController extends Controller
 {
     public function send()
     {
+        $user = Auth::user();
+
         $sendObject = new \stdClass;
         $sendObject->sender = 'Admin';
-        // $sendObject->receiver = 'alspok';
-        $sendObject->receiver = Auth::user()->name;
-        Mail::to(Auth::user()->email)->send(new FoodEmail($sendObject));
+        $sendObject->receiver = $user->name;
+        $sendObject->tableView = DB::table('products')->get();
+        
+        Mail::to($user->email)->send(new FoodEmail($sendObject));
     }
 }
--- resources/views/mail/food_mail_html.blade.php
+++ resources/views/mail/food_mail_html.blade.php
@@ -1,7 +1,17 @@
-Hello <i>{{ $foodMail->receiver }}</i>,
-<p>This is a demo email for testing purposes! Also, it's the HTML version.</p>
- 
-
-Thank You,
-<br/>
+Labas, <i>{{ $foodMail->receiver }}</i>,
+<p>Užsakymas</p>
+<p>Maistas</p>
+    @foreach($foodMail->tableView as $item)
+        @if($item->shop_id == 2 && $item->category == 1)
+            {{$item->name}}<br>
+        @endif
+    @endforeach
+<p>Kita</p>
+    @foreach($foodMail->tableView as $item)
+        @if($item->shop_id == 1 && $item->category == 2)
+            {{$item->name}}<br>
+        @endif
+    @endforeach
+<br><br>
+Dėkui.<br>
 <i>{{ $foodMail->sender }}</i>
