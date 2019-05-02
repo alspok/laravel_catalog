199b6653674b62de320a8d6dcf8b9b7999e8574d
alvydas <alspok@gmail.com>
2019-05-02 17:00:24 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::view('/food', 'food');
 Route::view('admin/layouts', 'food');
 Route::view('/logout', 'login');
 Route::view('/layouts', 'food');
-Route::view('mail/send', 'login');
 
 Route::resource('register', 'Auth\RegisterController');
 
@@ -44,5 +43,6 @@ Route::post('admin/products/softdelete/{id}', 'ProductController@softDelete')->n
 
 Route::get('mail/send', 'MailController@send');
 Route::post('mail/send', 'MailController@send')->name('mail.send');
+Route::view('mail/send', 'food');
 
 Route::get('/home', 'HomeController@index')->name('home');
