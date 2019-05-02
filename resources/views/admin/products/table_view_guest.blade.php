b74f5e985c85c4d4095eff7557299edb8fac6fe5
alvydas <alspok@gmail.com>
2019-05-02 16:19:06 +0300

    .

--- resources/views/admin/products/table_view_guest.blade.php
+++ resources/views/admin/products/table_view_guest.blade.php
@@ -136,14 +136,14 @@
 
 <div class="container" style='border: solid black 0.5px'>
 	<div class="row">
-        <div class="col-md-8">
+        <div class="col-md-6">
 			<h4>Pastabos/Komentarai</h4>
 			<form class='' action='' method='post'>
 				<textarea name="comment" form="usrform" cols='60' rows='4'></textarea>
 				<input type='submit' name="usrform" />
 			</form>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-6">
 			<form class='' action='' method='post'>
 				<button type='submit'>Send</button>
 			</form>
