9e550da1dbac2616ccb3b5750810651fb068c71b
alvydas <alspok@gmail.com>
2019-05-08 17:29:15 +0300

    scroll bar removed

--- resources/views/admin/products/table_view_guest.blade.php
+++ resources/views/admin/products/table_view_guest.blade.php
@@ -7,6 +7,7 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/products.css') }}" >
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href="{{asset('public/css/products.css')}}">
 
 <div class="container">
 	<div class="row">
